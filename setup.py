6cc61e3db82cdd29b296eb797f36817aa5cd0e7b
runtime-error786 <mustafa782a@gmail.com>
2024-09-18 00:35:01 +0500

    all files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="QA_System",
+    version="0.0.1",
+    author="Mustafa Rizwan",
+    author_email="mustafa782a@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3==1.34.37","langchain_community","chromadb","langchain-groq","awscli","pypdf","faiss-cpu","langchain_aws"],
+)
